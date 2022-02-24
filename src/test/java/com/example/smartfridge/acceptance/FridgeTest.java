12dc004304ca08ec8c62760e2fe0271a46f7113a
dariofedes <dariofedes@protonmail.com>
2022-02-24 18:44:19 +0100

    initial commit

---
+++ src/test/java/com/example/smartfridge/acceptance/FridgeTest.java
@@ -0,0 +1,11 @@
+package com.example.smartfridge.acceptance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeTest {
+	@Test
+	void contextLoads() {
+	}
+}
