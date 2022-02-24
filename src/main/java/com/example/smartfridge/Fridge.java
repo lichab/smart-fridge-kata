e3c17f6f8dc5793751a8b5dbc4778951d152030d
dariofedes <dariofedes@protonmail.com>
2022-02-24 19:04:17 +0100

    red

---
+++ src/main/java/com/example/smartfridge/Fridge.java
@@ -0,0 +1,27 @@
+package com.example.smartfridge;
+
+public class Fridge {
+    public String showDisplay() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void signalFridgeDoorOpened() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void scanAddedItem(String item, String expiry, String condition) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void signalFridgeDoorClosed() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void simulateDayOver() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void scanRemovedItem(String item) {
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/com/example/smartfridge/acceptance/FridgeTest.java
+++ src/test/java/com/example/smartfridge/acceptance/FridgeTest.java
@@ -1,11 +1,67 @@
 package com.example.smartfridge.acceptance;
 
+import com.example.smartfridge.Fridge;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FridgeTest {
 	@Test
-	void contextLoads() {
+	void tracksAndDisplaysAddedItemsExpiry() {
+
+		//Given
+		String expectedOutput = "EXPIRED: Milk\n" +
+				"Lettuce: 0 days remaining\n" +
+				"Peppers: 1 day remaining\n" +
+				"Cheese: 31 days remaining";
+		Fridge fridge = new Fridge();
+
+		// When
+		fridge.signalFridgeDoorOpened();
+		fridge.scanAddedItem("Milk", "21/10/21", "sealed");
+		fridge.scanAddedItem("Cheese", "18/11/21", "sealed");
+		fridge.scanAddedItem("Beef", "20/10/21", "sealed");
+		fridge.scanAddedItem("Lettuce", "22/10/21", "sealed");
+		fridge.signalFridgeDoorClosed();
+
+		fridge.simulateDayOver();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.signalFridgeDoorClosed();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.signalFridgeDoorClosed();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.scanRemovedItem("Milk");
+		fridge.signalFridgeDoorClosed();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.scanAddedItem("Milk", "26/10/21", "opened");
+		fridge.scanAddedItem("Peppers", "23/10/21", "opened");
+		fridge.signalFridgeDoorClosed();
+
+		fridge.simulateDayOver();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.scanRemovedItem("Beef");
+		fridge.scanRemovedItem("Lettuce");
+		fridge.signalFridgeDoorClosed();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.scanAddedItem("Lettuce", "22/10/21", "opened");
+		fridge.signalFridgeDoorClosed();
+
+		fridge.signalFridgeDoorOpened();
+		fridge.signalFridgeDoorClosed();
+
+		fridge.simulateDayOver();
+		
+		String actualOutput = fridge.showDisplay();
+
+		// Then
+		assertEquals(expectedOutput, actualOutput);
 	}
 }
